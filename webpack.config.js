bd1cf00808c349bfa4c1e80f5581c8d954416886
pufei <pufei007@163.com>
2017-07-04 17:25:54 +0800

    第二次提交

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var config= {
+    //页面入口文件配置
+    entry: './src/page/index/index.js',
+    //入口文件输出配置
+    output: {
+        path: './dist',
+        filename: 'app.js'
+    }
+};
+
+module.exports =config;
